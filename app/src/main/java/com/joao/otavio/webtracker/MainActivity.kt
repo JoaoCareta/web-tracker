44fcaeb268867d5eacaea8de6cc7343bd2c819ce
João Otávio Cesco Careta <joao.otavio@syngenta.com>
2025-05-14 14:12:23 -0300

    fix: ajuste #rc

--- app/src/main/java/com/joao/otavio/webtracker/MainActivity.kt
+++ app/src/main/java/com/joao/otavio/webtracker/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WebTrackerTheme {
-        Greeting("Android-test-2")
+        Greeting("Android-test")
     }
 }
