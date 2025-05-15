c285582d41f54b165ea75a8df0525235f9d4bff6
João Otávio Cesco Careta <joao.otavio@syngenta.com>
2025-05-15 15:56:50 -0300

    [WT10-0003] - Testando versionamento no firebase

--- app/src/main/java/com/joao/otavio/webtracker/MainActivity.kt
+++ app/src/main/java/com/joao/otavio/webtracker/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WebTrackerTheme {
-        Greeting("Android-test-with-testers")
+        Greeting("Android-test-with-firebase-testers")
     }
 }
