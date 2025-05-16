68511a7148e66f827b1860bb1f3a07322ea9ad7b
João Otávio Cesco Careta <joao.otavio@syngenta.com>
2025-05-16 11:53:36 -0300

    [WT10-0005] - Configuração do crashlytics no projeto.

---
+++ buildSrc/src/main/java/dependencies/firebase/Firebase.kt
@@ -0,0 +1,6 @@
+package dependencies.firebase
+
+object Firebase {
+    const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics"
+    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics"
+}
