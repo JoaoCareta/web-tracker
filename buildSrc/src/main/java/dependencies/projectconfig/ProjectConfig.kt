6b0a92412254214d5566cb18d4311bb39f7c062c
João Otávio Cesco Careta <joao.otavio@syngenta.com>
2025-06-26 07:18:52 -0300

    Defect/fix pipeline 2 (#69) #RC

--- buildSrc/src/main/java/dependencies/projectconfig/ProjectConfig.kt
+++ buildSrc/src/main/java/dependencies/projectconfig/ProjectConfig.kt
@@ -31,11 +31,9 @@ object ProjectConfig {
      */
     const val KEY_ALIAS = "KEY_ALIAS"
     const val KEY_PASSWORD = "KEY_PASSWORD"
-    const val STORE_FILE = "STORE_FILE"
     const val STORE_PASSWORD = "STORE_PASSWORD"
     const val KEYSTORE_PATH = "KEYSTORE_PATH"
     const val LOCAL_KEYSTORE_PATH = "keystore"
-    const val REMOTE_KEYSTORE_PATH = "keystore/release.keystore"
 
     /**
      * Build Types
